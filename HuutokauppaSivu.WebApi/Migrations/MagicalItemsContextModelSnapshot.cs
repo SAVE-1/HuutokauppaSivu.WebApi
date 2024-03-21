b89e1173c1a6352342fd3e3047e2c6b451e73d74
Default <samiv360@gmail.com>
2024-03-21 11:22:32 +0200

    Change line endings

--- HuutokauppaSivu.WebApi/Migrations/MagicalItemsContextModelSnapshot.cs
+++ HuutokauppaSivu.WebApi/Migrations/MagicalItemsContextModelSnapshot.cs
@@ -30,6 +30,9 @@ namespace HuutokauppaSivu.WebApi.Migrations
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
 
+                    b.Property<string>("CreatedBy")
+                        .HasColumnType("nvarchar(max)");
+
                     b.Property<string>("DeleteIdentification")
                         .HasColumnType("nvarchar(max)");
 
