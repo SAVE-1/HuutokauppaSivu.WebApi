cc622452012f19d10f117cbf6634935955c4bc5e
Default <samiv360@gmail.com>
2024-04-09 11:51:45 +0300

    Add a field for subclassing
    Allows for one-to-one relationship subclassing

--- HuutokauppaSivu.WebApi/Models/CategoryLookup.cs
+++ HuutokauppaSivu.WebApi/Models/CategoryLookup.cs
@@ -10,6 +10,25 @@ public class CategoryLookup
 
     public int CategoryId { get; set; }
 
+    /*
+
+    This should allow subcategories, for example in this order:
+        Top
+            Sub
+                Sub
+                    Sub
+                        Sub
+            Sub
+            Sub
+        Top
+        Top
+        Top
+        Top
+            Sub
+
+    */
+    public int SubCategoryId { get; set; }
+
     public string Name { get; set; } = string.Empty;
 
 
