e96f38ce1ee6093d4a2ebeb8452c7c13596b23c2
Default <samiv360@gmail.com>
2024-04-09 11:53:24 +0300

    Image model

---
+++ HuutokauppaSivu.WebApi/Models/MagicalItemImage.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Huutokauppa_sivu.Server.Models;
+
+public class MagicalItemImage
+{
+    [JsonIgnore]
+    public int Id { get; set; }
+    public string? DeleteIdentificationReference { get; set; }
+    public string? Name { get; set; } = string.Empty;
+}
